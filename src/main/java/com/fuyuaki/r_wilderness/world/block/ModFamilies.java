72602dd8e2f47fa50b71e8a819f58e4ee7a20770
FuyuKyoshi <j-dauphinais@hotmail.com>
2025-12-11 11:41:54 -0600

    stuff pt2

--- src/main/java/com/fuyuaki/r_wilderness/world/block/ModFamilies.java
+++ src/main/java/com/fuyuaki/r_wilderness/world/block/ModFamilies.java
@@ -28,19 +28,6 @@ public class ModFamilies {private static final Map<Block, BlockFamily> MAP = Map
             .recipeUnlockedBy("has_planks")
             .getFamily();
 
-    public static final BlockFamily ALPINE_PLANKS = familyBuilder(RBlocks.ALPINE_PLANKS.get())
-            .button(RBlocks.ALPINE_BUTTON.get())
-            .fence(RBlocks.ALPINE_FENCE.get())
-            .fenceGate(RBlocks.ALPINE_FENCE_GATE.get())
-            .pressurePlate(RBlocks.ALPINE_PRESSURE_PLATE.get())
-            .sign(RBlocks.ALPINE_SIGN.get(), RBlocks.ALPINE_WALL_SIGN.get())
-            .slab(RBlocks.ALPINE_SLAB.get())
-            .stairs(RBlocks.ALPINE_STAIRS.get())
-            .door(RBlocks.ALPINE_DOOR.get())
-            .trapdoor(RBlocks.ALPINE_TRAPDOOR.get())
-            .recipeGroupPrefix("wooden")
-            .recipeUnlockedBy("has_planks")
-            .getFamily();
 
     private static BlockFamily.Builder familyBuilder(Block baseBlock) {
         BlockFamily.Builder blockfamily$builder = new BlockFamily.Builder(baseBlock);
